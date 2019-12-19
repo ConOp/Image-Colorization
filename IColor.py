25d9da87000d143fb7bd7401fd3e99f15e67b6c8
ConWarOp <spitha3@hotmail.com>
2019-12-19 16:40:50 +0200

    LABimages to list

--- IColor.py
+++ IColor.py
@@ -1,3 +1,12 @@
 from skimage import io, color
-rgb = io.imread(filename)
+LABImages=[]
+for i in range(1,19):
+    LABImages.append(color.rgb2lab(io.imread("Dataset/"+str(i)+".png")[:,:,:3])) #Read images then RGBA->RGB and RGB->LAB
+
+
+'''rgba = io.imread("Dataset/1.png")
+rgb=rgba[:,:,:3]
 lab = color.rgb2lab(rgb)
+
+io.imshow(rgb)
+io.show()'''
