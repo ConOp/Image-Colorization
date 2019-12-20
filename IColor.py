7fb4b153d66276e168bd61e38ee00b017bf1a738
ConWarOp <spitha3@hotmail.com>
2019-12-20 02:14:59 +0200

    Slic Segments

--- IColor.py
+++ IColor.py
@@ -1,6 +1,7 @@
 from skimage import io, color
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from skimage.segmentation import slic
 import numpy as np
 LABImages=[]
 for i in range(1,18):
@@ -16,9 +17,13 @@ ABimages.shape=(278528,2) # Reshape from 17,16384,2 to 278528,2
 
 
 
-kmean = KMeans(n_clusters = 4)
+'''kmean = KMeans(n_clusters = 4)
 kmean.fit(ABimages)
 ykmean = kmean.predict(ABimages)
 plt.scatter(ABimages[:,1],ABimages[:,0],c=ykmean,marker='.')
-plt.scatter(kmean.cluster_centers_[:,1],kmean.cluster_centers_[:,0],c='r',marker='+')
-plt.show()
+plt.scatter(kmean.cluster_centers_[:,1],kmean.cluster_centers_[:,0],c='r',marker='+')'''
+
+segments = []
+for i in range(len(LABImages)):
+    segments.append(slic(LABImages[i],n_segments=5 ,compactness=10))
+print (segments[13].shape)
